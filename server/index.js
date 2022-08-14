ba45a9cb27fd2888f0767967e0dbcc79c0888e13
Andrey <chertiav@gmail.com>
2022-08-14 15:28:48 +0200

    add nodemon

--- server/index.js
+++ server/index.js
@@ -1,51 +1,23 @@
-// const summ = require('./testModule');
-// console.log(summ(10,20));
+// const fs = require('fs');
+// const os = require('os');
+// const path = require('path');
+const http =  require('http');
 
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-// console.log(os.cpus());
-// fs.writeFile('test.txt','Good morning', (err) => {
-// 	if (err) throw err;
-// 	console.log('Writing has been done')
-// });
-// fs.writeFile('test.txt','Good evening', (err) => {
-// 	if (err) throw err;
-// 	console.log('Writing has been done')
-// });
+const host = '127.0.0.1';
+const port = 5000;
+const server = http.createServer((req, res) => {
 
-//!update
-	// fs.writeFile('test.txt','Good morning\n', (err) => {
-	// 	if (err) throw err;
-	// 	console.log('Writing has been done')
-	// });
-	// fs.appendFile('test.txt','Good evening\n', (err) => {
-	// 	if (err) throw err;
-	// 	console.log('Writing has been done')
-	// });
-//!read
-	// fs.readFile('test.txt', 'utf8', (err, data) => {
-	// 	if (err) throw err;
-	// 	console.log('__dirnme', __dirname);
-	// 	console.log(data);
-	// })
-//!delete
-	// fs.unlink('test.txt', (err) => {
-	// 	if (err) throw err;
-	// 	console.log('Deleting has been done');
-	// })
+	console.log('HEADERS', req.headers);
+	console.log('URL', req.url);
+	console.log('METHOD', req.method);
 
-
-//todo работа с каталогами __dirname
-fs.readFile('test.txt', 'utf8', (err, data) => {
-	if (err) throw err;
-	console.log('__dirnme', __dirname);
-	console.log(data);
+	res.statusCode = 200;
+	res.setHeader('UserId', 10);
+	res.setHeader('Content-Type', 'text/html, charset=utf-8');
+	res.write('<h1>We will rock you</h1>');
+	res.end();
 })
-//создание
-const newDir = path.join(__dirname, '/public')
-fs.mkdir(newDir, (err, data) => {
-	if (err) throw err;
-});
-
-
+//запуск сервера:
+server.listen(port, host, ()=>{
+	console.log(`Server has been started at ${host}:${port}`)
+})
